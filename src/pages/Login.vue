2481d8beca4a062d32263ea39294297415307d25
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2020-04-06 23:35:06 +0900

    feat: firebase installed

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,24 @@
 <template>
-  <h2>login page</h2>
+  <div>
+    <div class="jumbotron bg-primary text-white text-center">
+      <h2 class="lead display-3">#SLACK#</h2>
+      <p>Realtime communication at it's beast</p>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row mt-5">
+        <div class="col text-center">
+          <button class="btn btn-outline-danger btn-lg">Login with Google</button>
+        </div>
+      </div>
+
+      <div class="row mt-5">
+        <div class="col text-center">
+          <button class="btn btn-outline-info btn-lg">Login with Twitter</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +26,9 @@
     name: 'login'
   }
 </script>
+
+<style scoped>
+  .btn, .jumbotron {
+    border-radius: 0px;
+  }
+</style>
